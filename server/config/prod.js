a17bd9d383c97df0290689ddac3f0f6bfbc852aa
Adam Bohannon <abo46n2@gmail.com>
2017-12-14 21:30:45 -0800

    Initial commit

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
   shopName: process.env.SHOP_NAME,
   apiKey: process.env.API_KEY,
-  password: process.env.PASSWORD,
-};
+  password: process.env.PASSWORD
+}
--- server/controller.js
+++ server/controller.js
@@ -1,23 +1,23 @@
-const Shopify = require('shopify-api-node');
-const keys = require('./config/keys');
+const Shopify = require('shopify-api-node')
+const keys = require('./config/keys')
 
 const shopify = new Shopify({
   shopName: keys.shopName,
   apiKey: keys.apiKey,
-  password: keys.password,
-});
+  password: keys.password
+})
 
 module.exports = {
-  greeting(req, res) {
-    res.send({ hello: 'world' });
+  greeting (req, res) {
+    res.send({ hello: 'world' })
   },
 
-  getCollections(req, res) {
+  getCollections (req, res) {
     shopify.collectionListing.list()
       .then(collectionData => res.send(collectionData))
       .catch((err) => {
-        console.log(err);
-        res.send('404 Not Found');
-      });
-  },
-};
+        console.log(err)
+        res.send('404 Not Found')
+      })
+  }
+}
