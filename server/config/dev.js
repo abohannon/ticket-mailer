6a978244f873d448a216297e9c5cf55aaae80ca3
Adam Bohannon <abo46n2@gmail.com>
2018-01-03 10:27:44 -0800

    Added live shop credentials for testing

--- server/config/dev.js
+++ server/config/dev.js
@@ -5,3 +5,12 @@ module.exports = {
   mongoURI: 'mongodb://admin:wBIFn8!Pu70b@ds133162.mlab.com:33162/ticket-mailer-dev',
   cookieKey: 'asdfjkh19876324'
 }
+
+// XXX: FOR TESTING
+// module.exports = {
+//   shopName: 'showstubs',
+//   apiKey: '6faa7dde0941ad14801974f27c2e5e9f',
+//   password: 'f2a582abd381ca645d4eed1de9a7ea12',
+//   mongoURI: 'mongodb://admin:wBIFn8!Pu70b@ds133162.mlab.com:33162/ticket-mailer-dev',
+//   cookieKey: 'asdfjkh19876324'
+// }
