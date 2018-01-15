413269cb522f9aa57767a9c1c89a032d9ab481e0
Adam Bohannon <abo46n2@gmail.com>
2018-01-15 12:05:58 -0800

    Added refresh indicator to all pages that fetch data

--- server/client/src/reducers/shopifyFetchReducer.js
+++ server/client/src/reducers/shopifyFetchReducer.js
@@ -30,101 +30,149 @@ const initialState = {
 
 export default function (state = initialState, action) {
   switch (action.type) {
-    case FETCH_ORDERS_PENDING: {
+    case FETCH_ORDERS_PENDING:
+    {
       const newState = {
         fetchOrdersPending: action,
         fetchOrdersRejected: undefined,
         fetchOrdersSuccess: undefined,
       };
-      return { ...state, ...newState };
+      return {
+        ...state,
+        ...newState,
+      };
     }
-    case FETCH_ORDERS_REJECTED: {
+    case FETCH_ORDERS_REJECTED:
+    {
       const newState = {
         fetchOrdersPending: undefined,
         fetchOrdersRejected: action,
         fetchOrdersSuccess: undefined,
       };
-      return { ...state, ...newState };
+      return {
+        ...state,
+        ...newState,
+      };
     }
-    case FETCH_ORDERS_SUCCESS: {
+    case FETCH_ORDERS_SUCCESS:
+    {
       const newState = {
         fetchOrdersPending: undefined,
         fetchOrdersRejected: undefined,
         fetchOrdersSuccess: action,
       };
-      return { ...state, ...newState };
+      return {
+        ...state,
+        ...newState,
+      };
     }
-    case FETCH_ALL_ORDERS_PENDING: {
+    case FETCH_ALL_ORDERS_PENDING:
+    {
       const newState = {
         fetchAllOrdersPending: action,
         fetchAllOrdersRejected: undefined,
         fetchAllOrdersSuccess: undefined,
       };
-      return { ...state, ...newState };
+      return {
+        ...state,
+        ...newState,
+      };
     }
-    case FETCH_ALL_ORDERS_REJECTED: {
+    case FETCH_ALL_ORDERS_REJECTED:
+    {
       const newState = {
         fetchAllOrdersPending: undefined,
         fetchAllOrdersRejected: action,
         fetchAllOrdersSuccess: undefined,
       };
-      return { ...state, ...newState };
+      return {
+        ...state,
+        ...newState,
+      };
     }
-    case FETCH_ALL_ORDERS_SUCCESS: {
+    case FETCH_ALL_ORDERS_SUCCESS:
+    {
       const newState = {
         fetchAllOrdersPending: undefined,
         fetchAllOrdersRejected: undefined,
         fetchAllOrdersSuccess: action,
       };
-      return { ...state, ...newState };
+      return {
+        ...state,
+        ...newState,
+      };
     }
-    case FETCH_PRODUCTS_PENDING: {
+    case FETCH_PRODUCTS_PENDING:
+    {
       const newState = {
         fetchProductsPending: action,
         fetchProductsRejected: undefined,
         fetchProductsSuccess: undefined,
       };
-      return { ...state, ...newState };
+      return {
+        ...state,
+        ...newState,
+      };
     }
-    case FETCH_PRODUCTS_REJECTED: {
+    case FETCH_PRODUCTS_REJECTED:
+    {
       const newState = {
         fetchProductsPending: undefined,
         fetchProductsRejected: action,
         fetchProductsSuccess: undefined,
       };
-      return { ...state, ...newState };
+      return {
+        ...state,
+        ...newState,
+      };
     }
-    case FETCH_PRODUCTS_SUCCESS: {
+    case FETCH_PRODUCTS_SUCCESS:
+    {
       const newState = {
         fetchProductsPending: undefined,
         fetchProductsRejected: undefined,
         fetchProductsSuccess: action,
       };
-      return { ...state, ...newState };
+      return {
+        ...state,
+        ...newState,
+      };
     }
-    case FETCH_COLLECTIONS_PENDING: {
+    case FETCH_COLLECTIONS_PENDING:
+    {
       const newState = {
         fetchCollectionsPending: action,
         fetchCollectionsRejected: undefined,
         fetchCollectionsSuccess: undefined,
       };
-      return { ...state, ...newState };
+      return {
+        ...state,
+        ...newState,
+      };
     }
-    case FETCH_COLLECTIONS_REJECTED: {
+    case FETCH_COLLECTIONS_REJECTED:
+    {
       const newState = {
         fetchCollectionsPending: undefined,
         fetchCollectionsRejected: action,
         fetchCollectionsSuccess: undefined,
       };
-      return { ...state, ...newState };
+      return {
+        ...state,
+        ...newState,
+      };
     }
-    case FETCH_COLLECTIONS_SUCCESS: {
+    case FETCH_COLLECTIONS_SUCCESS:
+    {
       const newState = {
         fetchCollectionsPending: undefined,
         fetchCollectionsRejected: undefined,
         fetchCollectionsSuccess: action,
       };
-      return { ...state, ...newState };
+      return {
+        ...state,
+        ...newState,
+      };
     }
     default:
       return state;
