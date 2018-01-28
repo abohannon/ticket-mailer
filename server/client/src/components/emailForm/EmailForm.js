2e66d418fd9c6972098590299ab0857b1be03083
Adam Bohannon <abo46n2@gmail.com>
2018-01-28 12:51:05 -0800

    Refactored form component

--- server/client/src/components/emailForm/EmailForm.js
+++ server/client/src/components/emailForm/EmailForm.js
@@ -11,8 +11,15 @@ import { ACCENT_BLUE, WHITE } from '../../style/constants';
 const EmailFormStyles = () => ({
   formContainer: {
     display: 'flex',
+    width: 600,
     minWidth: 350,
     paddingLeft: 16,
+  },
+  formStyle: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    width: 600,
   },
   flexRow: {
     display: 'flex',
@@ -48,11 +55,18 @@ class EmailForm extends Component {
   }
 
   render() {
-    const { formContainer, flexRow, buttonContainer, blue } = EmailFormStyles();
+    const {
+      formContainer,
+      formStyle,
+      flexRow,
+      buttonContainer,
+      blue,
+    } = EmailFormStyles();
 
     const {
       checkIn,
       startTime,
+      eventNotes,
       pickup,
       shipping,
       shippingDate,
@@ -62,10 +76,26 @@ class EmailForm extends Component {
 
     const { history } = this.props;
 
+    const form = formFields.map(field => (
+      <Field
+        component={EmailTextField}
+        type="text"
+        label={field.label}
+        name={field.name}
+        multiLine={field.multiLine}
+        fullWidth={field.fullWidth}
+        style={field.style}
+      />
+    ));
+
     return (
       <div className="email-form__container" style={formContainer}>
-        <form onSubmit={this.props.handleSubmit(this.props.onFormSubmit)}>
-          <div style={flexRow}>
+        <form
+          onSubmit={this.props.handleSubmit(this.props.onFormSubmit)}
+          style={formStyle}
+        >
+          {form}
+          {/* <div style={flexRow}>
             <Field
               component={EmailTextField}
               type="text"
@@ -78,8 +108,8 @@ class EmailForm extends Component {
               label={startTime.label}
               name={startTime.name}
             />
-          </div>
-          <div>
+            </div>
+            <div>
             <Field
               component={EmailTextField}
               type="text"
@@ -87,8 +117,8 @@ class EmailForm extends Component {
               name={pickup.name}
               multiLine={pickup.multiLine}
             />
-          </div>
-          <div style={flexRow}>
+            </div>
+            <div style={flexRow}>
             <Field
               component={EmailTextField}
               type="text"
@@ -102,8 +132,8 @@ class EmailForm extends Component {
               label={shippingDate.label}
               name={shippingDate.name}
             />
-          </div>
-          <div style={flexRow}>
+            </div>
+            <div style={flexRow}>
             <Field
               component={EmailTextField}
               type="text"
@@ -117,7 +147,7 @@ class EmailForm extends Component {
               label={digitalDate.label}
               name={digitalDate.name}
             />
-          </div>
+          </div> */}
           <div className="email-form__button-container" style={buttonContainer}>
             <FlatButton label="Go Back" onClick={history.goBack} />
             <FlatButton
